5ed820a6b941ef30cd1b0f3bbb1776607a28c603
Aarayan84 <aarayan2005@gmail.com>
2026-07-14 21:35:20 +0530

    update the app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 
 
 app.get("/",(req,res)=>{
-    req.json({
+    res.json({
         success:true,
         message : "ERP API Running Successfully"
     });
